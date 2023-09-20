677ee5ce32a21016cca4f5f4944a33054c55db4b
Israel Abebe <se.israel.abebe@gmail.com>
2023-09-20 20:54:25 +0200

    updated openai

--- privateGPT.py
+++ privateGPT.py
@@ -4,7 +4,7 @@ from langchain.chains import RetrievalQA
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.vectorstores import Chroma
-from langchain.llms import GPT4All, LlamaCpp
+from langchain.llms import GPT4All, LlamaCpp, OpenAI
 import chromadb
 import os
 import argparse
@@ -14,25 +14,27 @@ from omegaconf import OmegaConf
 
 from constants import CHROMA_SETTINGS
 
-def main(embeddings_model_name, persist_directory, model_type, model_path, model_n_ctx, model_n_batch, target_source_chunks):
+def main(embeddings_model_name, persist_directory, model_type, model_path, model_n_ctx, model_n_batch, target_source_chunks,mute_stream,hide_source):
     # Parse the command line arguments
     embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
     chroma_client = chromadb.PersistentClient(settings=CHROMA_SETTINGS , path=persist_directory)
     db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS, client=chroma_client)
     retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
     # activate/deactivate the streaming StdOut callback for LLMs
-    callbacks = [] if not conf.get('mute_stream') else [StreamingStdOutCallbackHandler()]
+    callbacks = [] if mute_stream else [StreamingStdOutCallbackHandler()]
     # Prepare the LLM
     match model_type:
         case "LlamaCpp":
             llm = LlamaCpp(model_path=model_path, max_tokens=model_n_ctx, n_batch=model_n_batch, callbacks=callbacks, verbose=False)
         case "GPT4All":
             llm = GPT4All(model=model_path, max_tokens=model_n_ctx, backend='gptj', n_batch=model_n_batch, callbacks=callbacks, verbose=False)
+        case "OpenAI":
+            llm = OpenAI()
         case _default:
             # raise exception if model_type is not supported
             raise Exception(f"Model type {model_type} is not supported. Please choose one of the following: LlamaCpp, GPT4All")
 
-    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents= conf.get('hide_source')==None)
+    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents= hide_source)
     # Interactive questions and answers
     while True:
         query = input("\nEnter a query: ")
@@ -44,7 +46,7 @@ def main(embeddings_model_name, persist_directory, model_type, model_path, model
         # Get the answer from the chain
         start = time.time()
         res = qa(query)
-        answer, docs = res['result'], [] if not conf.get('hide_source') else res['source_documents']
+        answer, docs = res['result'], [] if hide_source else  res['source_documents']
         end = time.time()
 
         # Print the result
@@ -53,10 +55,10 @@ def main(embeddings_model_name, persist_directory, model_type, model_path, model
         print(f"\n> Answer (took {round(end - start, 2)} s.):")
         print(answer)
 
-        # # Print the relevant sources used for the answer
-        # for document in docs:
-        #     print("\n> " + document.metadata["source"] + ":")
-        #     print(document.page_content)
+        # Print the relevant sources used for the answer
+        for document in docs:
+            print("\n> " + document.metadata["source"] + ":")
+            print(document.page_content)
 
 
 
@@ -77,6 +79,11 @@ if __name__ == "__main__":
     model_n_batch = int(conf.get('MODEL_N_BATCH',os.environ.get('MODEL_N_BATCH',8)))
     target_source_chunks = int(conf.get('TARGET_SOURCE_CHUNKS', os.environ.get('TARGET_SOURCE_CHUNKS',4))) 
 
-    # python privateGPT.py PERSIST_DIRECTORY=db1
+    mute_stream=conf.get('mute_stream', False)
+    hide_source=conf.get('hide_source', True)
+
+    
+
+    # python privateGPT.py PERSIST_DIRECTORY=files/db1 MODEL_TYPE=OpenAI  MODEL_PATH=files/models/ggml-gpt4all-j-v1.3-groovy.bin  mute_stream=False hide_source=True
     print(embeddings_model_name, persist_directory, model_type, model_path, model_n_ctx, model_n_batch, target_source_chunks)
-    main(embeddings_model_name, persist_directory, model_type, model_path, model_n_ctx, model_n_batch, target_source_chunks)
+    main(embeddings_model_name, persist_directory, model_type, model_path, model_n_ctx, model_n_batch, target_source_chunks,mute_stream,hide_source)
